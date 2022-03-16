45a99afce470a5e89c85990bce0ebbb5f3868ad1
Hiagoleresdev <hiagoleresdev@gmail.com>
2022-03-16 12:22:23 -0300

    Exercício 1 e 2 terminados

---
+++ Funcoes.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioF360
+{
+    public class Funcoes
+    {
+
+        string[] letrasFoo = { "s","l","f","w","k"};
+        string[] letrasBar = { "a","b","c","e","g","h","i","j","m","n","o","p","q","r","t","u","v","x","y","z"};
+
+       
+        char[] ordemAlfabetica = {'k','b','w','r','q','d','n','f','x','j','m','l','v','h','t','c','g','z','p','s'};
+
+        
+        public int verbos, verbosPrimeiraPessoa=0;
+
+
+        public int acharPreposicao()
+        {
+            int palavras = 0;
+            using (StreamReader sr = new StreamReader(@"C:\Users\hiago.leres\source\repos\DesafioF360\textoB.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+                  
+                    string line = sr.ReadLine();
+                    string[] words = line.Split();
+                    foreach (string word in words)
+                    {
+                       if(word.Length == 3)
+                        {
+                           if( word[2] != 's' && word[2] != 'l' && word[2] != 'f' &&
+                               word[2] != 'w' && word[2] != 'k' &&  word[0] != 'd' && word[1] != 'd' && word[2] != 'd')
+                            {
+                                palavras++;
+                            }
+                        }
+                       
+                    }
+                   
+                }
+                return palavras;
+            }
+        }
+
+        public int acharVerbos()
+        {
+
+            using (StreamReader sr = new StreamReader(@"C:\Users\hiago.leres\source\repos\DesafioF360\textoB.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+
+                    string line = sr.ReadLine();
+                    string[] words = line.Split();
+                    foreach (string word in words)
+                    {
+                        if (word.Length >= 8)
+                        {
+                            //Verifica verbos
+                            if (word[7].Equals('s') || word[7].Equals('l') || word[7].Equals('f') || word[7].Equals('w') || word[7].Equals('k'))
+                            {
+                                //Console.WriteLine(word);
+                                this.verbos++;
+
+                            }
+                            //Verifica verbos maiusculos
+                            if (word.Length >= 8 && word[0] != 's' && word[0] != 'l' && word[0] != 'f' && word[0] != 'w' && word[0] != 'k' && (
+                                word[7].Equals('s') || word[7].Equals('l') || word[7].Equals('f') || word[7].Equals('w') || word[7].Equals('k')))
+                            {
+
+                                this.verbosPrimeiraPessoa++;
+                            }
+                        }
+
+                    }
+
+                }
+                return 0;
+            }
+           
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,23 @@ namespace DesafioF360
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            Funcoes f = new Funcoes();
+
+            int preposicoes = f.acharPreposicao();
+
+            int acharVerbos = f.acharVerbos();
+            int verbos = f.verbos;
+            int verbosPrimeiraPessoa = f.verbosPrimeiraPessoa;
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("----------------DESAFIO F°360----------------");
+            Console.WriteLine("1-) E no Texto B, quantas preposições existem?");
+            Console.WriteLine("R:" + preposicoes);
+            Console.WriteLine("2-) Ja no texto B, quantos são os verbos");
+            Console.WriteLine("R:" + verbos);
+            Console.WriteLine("3-) E quantos verbos no texto B estão em primeira pessoa?");
+            Console.WriteLine("R:" + verbosPrimeiraPessoa);
+
         }
     }
 }
