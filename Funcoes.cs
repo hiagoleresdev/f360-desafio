463ae1ccd0a61beda9b7f8245c1d6d09df7924e9
Hiagoleresdev <hiagoleresdev@gmail.com>
2022-03-18 12:49:09 -0300

    Finalizando o desafio

--- Funcoes.cs
+++ Funcoes.cs
@@ -9,16 +9,12 @@ namespace DesafioF360
     public class Funcoes
     {
 
-        string[] letrasFoo = { "s","l","f","w","k"};
-        string[] letrasBar = { "a","b","c","e","g","h","i","j","m","n","o","p","q","r","t","u","v","x","y","z"};
-
-       
-        char[] ordemAlfabetica = {'k','b','w','r','q','d','n','f','x','j','m','l','v','h','t','c','g','z','p','s'};
+        public int verbos, verbosPrimeiraPessoa = 0;
+        char[] alfabeto = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y' };
+        char[] alfabetoKlingon = { 'k', 'b', 'w', 'r', 'q', 'd', 'n', 'f', 'x', 'j', 'm', 'l', 'v', 'h', 't', 'c', 'g', 'z', 'p', 's' };
 
         
-        public int verbos, verbosPrimeiraPessoa=0;
-
-
+        //MÉTODO PARA ACHAR PREPOSIÇÕES
         public int acharPreposicao()
         {
             int palavras = 0;
@@ -47,6 +43,7 @@ namespace DesafioF360
             }
         }
 
+        //MÉTODO PARA ACHAR OS VERBOS
         public int acharVerbos()
         {
 
@@ -64,9 +61,7 @@ namespace DesafioF360
                             //Verifica verbos
                             if (word[7].Equals('s') || word[7].Equals('l') || word[7].Equals('f') || word[7].Equals('w') || word[7].Equals('k'))
                             {
-                                //Console.WriteLine(word);
                                 this.verbos++;
-
                             }
                             //Verifica verbos maiusculos
                             if (word.Length >= 8 && word[0] != 's' && word[0] != 'l' && word[0] != 'f' && word[0] != 'w' && word[0] != 'k' && (
@@ -85,5 +80,116 @@ namespace DesafioF360
            
         }
 
+        //MÉTODO PARA RETIRAR PALAVRAS REPETIDAS
+        public string[] textoFinal()
+        {
+            var textoFinal = new StringBuilder("");
+            using (StreamReader sr = new StreamReader(@"C:\Users\hiago.leres\source\repos\DesafioF360\textoB.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+
+                    string line = sr.ReadLine();
+                    string[] words = line.Split();
+
+                    foreach (string word in words)
+                    {
+                        if (!textoFinal.ToString().Contains($" {word} "))
+                        {
+                            if (textoFinal.Length == 0)
+                            {
+                                textoFinal.Append(word);
+                            }
+                            else
+                            {
+                                textoFinal.Append(" ");
+                                textoFinal.Append(word);
+                            }
+                        }
+                    }
+
+                }
+              
+                return textoFinal.ToString().Split(" ");
+            }
+
+            
+        }
+
+        //MÉTODO PARA ORDENAR NA ORDEM DO ALFABETO KLINDON
+
+        /*
+        public string TextoOrdenado()
+        {
+           
+            var vocabulario = new Dictionary<string, string>();
+            var textoOrdenado = new StringBuilder("");
+            var ordemLetras = new StringBuilder("");
+
+
+            using (StreamReader sr = new StreamReader(@"C:\Users\hiago.leres\source\repos\DesafioF360\textoB.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+
+                    string line = sr.ReadLine();
+                    string[] words = line.Split();
+                    foreach (string word in words)
+                    {
+
+                        ordemLetras.Clear();
+                        foreach (var letter in word)
+                        {
+                            ordemLetras.Append(alfabeto[Array.IndexOf(alfabetoKlingon, letter)]);
+                        }
+                        vocabulario.Add(word, ordemLetras.ToString());
+
+
+                        var vocabularioOrdenado = vocabulario.OrderBy(x => x.Value);
+                        foreach (var item in vocabularioOrdenado)
+                        {
+                            textoOrdenado.Append(item.Key + " ");
+                        }
+                        textoOrdenado.Remove(textoOrdenado.Length - 1, 1);
+                    }
+                    
+                }
+                return textoOrdenado.ToString();
+            }
+
+
+        }*/
+
+        //Métodos para achar os números bonitos
+        public int Numeros()
+        {
+            int qtdeNumeros = 0;
+            using (StreamReader sr = new StreamReader(@"C:\Users\hiago.leres\source\repos\DesafioF360\textoB.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    string[] words = line.Split();
+
+                    foreach (string word in words)
+                    {
+                        long numero = 0;
+                        long peso = 1;
+                        foreach (var letter in word)
+                        {
+                            numero += Array.IndexOf(alfabetoKlingon, letter) * peso;
+                            peso *= 20;
+                        }
+
+                        if (numero >= 440566 && numero % 3 == 0)
+                        {
+                            qtdeNumeros++;
+                        }
+                    }
+
+                }
+                return qtdeNumeros;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace DesafioF360
             Funcoes f = new Funcoes();
 
             int preposicoes = f.acharPreposicao();
-
             int acharVerbos = f.acharVerbos();
             int verbos = f.verbos;
             int verbosPrimeiraPessoa = f.verbosPrimeiraPessoa;
@@ -17,10 +16,17 @@ namespace DesafioF360
             Console.WriteLine("----------------DESAFIO F°360----------------");
             Console.WriteLine("1-) E no Texto B, quantas preposições existem?");
             Console.WriteLine("R:" + preposicoes);
-            Console.WriteLine("2-) Ja no texto B, quantos são os verbos");
+            Console.WriteLine("2-) Ja no Texto B, quantos são os verbos");
             Console.WriteLine("R:" + verbos);
             Console.WriteLine("3-) E quantos verbos no texto B estão em primeira pessoa?");
             Console.WriteLine("R:" + verbosPrimeiraPessoa);
+            Console.WriteLine("4: Como seria a lista de vocabulário do Texto B?");
+            //Console.WriteLine(f.TextoOrdenado());
+            Console.WriteLine("5: E no Texto B? Quantos números bonitos existem?");
+            Console.WriteLine(f.Numeros());
+            Console.WriteLine("---------------------------------------------");
+
+
 
         }
     }
